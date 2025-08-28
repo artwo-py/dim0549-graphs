4a7cd306bdf6d31f13cff1046eda0e5b21d8f916
Caio2a7 <caiodanielfonseca@gmail.com>
2025-08-28 09:45:30 -0300

    chore: adicionad dockerfile, compose e refatorado alguns arquivos e pastas

--- 1-unidade/1-1/q2.py
+++ 1-unidade/1-1/q2.py
@@ -1,5 +1,7 @@
 from classes import AdjacencyList
 
+CLI_LINE_LENGHT=25
+
 graph_order = None
 graph_order = int(input("Insira a ordem do grafo: "))
 
@@ -14,14 +16,15 @@ if graph_order:
         adj_matrix.append(row)
 
 while True:
-    print("\n====Menu====")
+    print("\n====MENU GRAFOS-CLI====")
     print("1. Adicionar uma aresta")
     print("2. Exibir matriz de adjacência")
     print("3. Exibir lista de adjacência")
-    print("6. Sair")
-    option = input("Escolha uma opção: ")
+    print("4. Sair")
+    option = input("(Escolha uma opção): ")
 
     if option == '1':
+        print("-"*CLI_LINE_LENGHT)
         v1 = int(input("Insira o ID do primeiro vértice: "))
         v2 = int(input("Insira o ID do segundo vértice: "))
 
@@ -30,12 +33,14 @@ while True:
             adj_matrix[v2-1][v1-1] = 1
 
     elif option == '2':
+        print("-"*CLI_LINE_LENGHT)
         for i in range(graph_order):
             for j in range(graph_order):
                 print(adj_matrix[i][j], end=" ")
             print()
     
     elif option == '3':
+        print("-"*CLI_LINE_LENGHT)
         adj_list = [AdjacencyList(v) for v in range(1, graph_order + 1)]
         for i in range(graph_order):
             for j in range(i, graph_order):
